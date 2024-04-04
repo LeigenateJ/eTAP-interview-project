9941742d19269c57af0a9ce9645aeac2c18f2765
Jingyuan Dong <87741275+Leigenate@users.noreply.github.com>
2024-04-04 22:29:13 +1300

    add models on backend

--- database/execute_sql.php
+++ database/execute_sql.php
@@ -1,18 +1,17 @@
 <?php
-$host = 'localhost'; // 数据库服务器
-$user = 'Jenkin'; // 数据库用户名
-$pass = 'Djy19960724'; // 数据库密码
+$host = 'localhost'; // db server
+$user = 'Jenkin'; // db user name
+$pass = 'Djy19960724'; // db psw
 
-// 先连接到MySQL，不指定数据库
 $pdo = new PDO("mysql:host=$host", $user, $pass);
 
-// 读取SQL文件
-$sql = file_get_contents('./initial.sql'); // 确保路径正确
+// read sql file
+$sql = file_get_contents('./initial.sql');
 
-// 分离SQL语句
+// seperate sql queries
 $queries = array_filter(array_map('trim', explode(';', $sql)));
 
-// 执行SQL语句
+// exec queries
 foreach ($queries as $query) {
     if (!$pdo->query($query)) {
         $errorInfo = $pdo->errorInfo();
@@ -21,4 +20,3 @@ foreach ($queries as $query) {
 }
 
 echo "Database and tables created successfully!";
-?>
